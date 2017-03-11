68abec8164ce221f114408a55674ad916bff36fd
volodymyr vandych <vandychvolodymyr@gmail.com>
2017-03-11 18:03:08 +0300

    1lesson

--- lesson_1/main1.py
+++ lesson_1/main1.py
@@ -3,5 +3,5 @@ speed_of_light=299792458 #meter per second
 bilionth=1.0/1000000000
 nanostik=speed_of_light*bilionth*100
 print nanostik
-cycles_per_second=2.7*bilionth #2.7 gHz processor
+cycles_per_second=2.7*bilionth #2.7 gHz 
 print speed_of_light/cycles_per_second
