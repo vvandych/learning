9c5bfb2ff6ee2e5167a6549365c3357361c28fde
volodymyr vandych <vandychvolodymyr@gmail.com>
2017-03-13 13:14:20 +0200

    Make a program # that checks if a word is a palindrome. # If the word is a palindrome, print 0 to the terminal, # -1 otherwise. # The word contains lowercase letters a-z and # will be at least one character long.

--- lesson_1/main.py
+++ lesson_1/main.py
@@ -8,3 +8,23 @@ part_a=line[:start]
 part_b=line[start+marker_end:]
 replaced=part_a+replacement+part_b
 print replaced
+
+
+
+
+word = "madam"
+# test case 2
+# word = "madman" # uncomment this to test
+
+###
+# YOUR CODE HERE. DO NOT DELETE THIS LINE OR ADD A word DEFINITION BELOW IT.
+###
+alphabet='abcdefghijklmnopqrstuvwxyz'
+a=(alphabet.find(word[0])-alphabet.find(word[-1]))
+b=-1
+c=1
+if  b<a<c:
+    is_palindrome=0
+else:
+    is_palindrome=(-1)
+print is_palindrome
