8c93471657852079a3f300e42ebbc156f87edf8e
volodymyr vandych <vandychvolodymyr@gmail.com>
2017-03-16 12:44:23 +0200

    2 quiz works in nrowser but doesnt work in studio

---
+++ lesson_1/lesson_2_problemset_2.py
@@ -0,0 +1,31 @@
+#weekend
+
+def weekend(day):
+    if day=='Saturday' or day=='Sunday':
+        return True
+    if day!='Saturday' or day!='Sunday':
+        return False
+    else:
+        return day
+print weekend('Monday')
+print weekend('Saturday')
+print weekend('July')
+
+
+
+#stamps
+def stamps(a):
+    while True:
+      b=int (a/5)
+      break
+    while True:
+        c=int ((a-b*5)/2)
+        break
+    while True:
+        d=int ((a-5*b-2*c)/1)
+        break
+    return b,c,d
+print stamps(8)
+print stamps(5)
+print stamps(29)
+print stamps(0)
